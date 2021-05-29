e463c8f7b3eeea1a6b1da8788ad2919d4e224b64
Rachel Hendricks <rachelhen92@gmail.com>
2021-05-29 14:05:23 -0600

    Update render method

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -6,7 +6,7 @@ class PokemonDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemonDetails: [],
+      pokemonDetails: '',
       id: props.id
     }
   }
@@ -15,15 +15,28 @@ class PokemonDetails extends Component {
     try {
       const fetchedPokemonDetails = await getIndividualPokemon(this.state.id)
       this.setState({ pokemonDetails: fetchedPokemonDetails})
+      console.log(this.state.pokemonDetails)
     } catch (e) {
       this.setState({error: 'Request failed'})
     }
   }
 
   render() {
-    console.log(this.state.pokemonDetails)
+    const { pokemonDetails, id } = this.state
     return(
-    <h1>PokemonDetails</h1>
+      <>
+      {pokemonDetails &&
+        <section>
+          <h1>{pokemonDetails.name}</h1>
+          <img src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} alt={pokemonDetails.name} />
+          <p>Weight: {pokemonDetails.weight}</p>
+          <p>Height: {pokemonDetails.height}</p>
+          <p>Base experience: {pokemonDetails.base_experience}</p>
+          <p>Type: {pokemonDetails.types[0].type.name}</p>
+          <p>Move: {pokemonDetails.moves[0].move.name}</p>
+        </section>
+      }
+      </>
     )
   }
 }
