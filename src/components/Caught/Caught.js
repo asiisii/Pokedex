989f59d778714ea32f771217041336df8c0c7ae4
Dustin-Har <harbaughdustin@yahoo.com>
2021-05-30 19:08:00 -0400

    Add functionality to caught.js

--- src/components/Caught/Caught.js
+++ src/components/Caught/Caught.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import uncaughtBall from '../../Assets/uncaughtBall.png';
+import caughtBall from  '../../Assets/caughtBall.png'
+import PokemonCard from '../PokemonCard/PokemonCard';
 
 export default function Caught({pokemons, caught, favorite}) {
   const caughtPokemon = pokemons.filter(critter => caught.includes(critter.name))
-  console.log(caughtPokemon);
+  const caughtPokemonCards = caughtPokemon.map((pokemon, index) => {
+    const getId = pokemon.url.split('/')[6];
+    const favoritePokemon = caught.includes(pokemon.name);
+    const soloPic = `https://pokeres.bastionbot.org/images/pokemon/${getId}.png`;
+    return (
+      <PokemonCard
+        favorite={favorite}
+        pokeball={!favoritePokemon ? uncaughtBall : caughtBall}
+        key = {getId}
+        id = {getId}
+        img = {soloPic}
+        name = {pokemon.name}
+      />
+    )
+  })
   return (
-    <div>
-      
+    <>
+    <div className="card-display">
+    {caughtPokemonCards}
     </div>
+    </>
   )
 }
+
