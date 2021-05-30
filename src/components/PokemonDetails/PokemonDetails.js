1aa3e39a34d544b8d4391a8989c75aee7965d98e
Ashish Malla <ashishmalla45@gmail.com>
2021-05-30 07:39:15 -0600

    Update fetch calls with interpolated arguments

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getIndividualPokemon } from '../../apiData/apiCalls';
+import fetchPokemonData  from '../../apiData/apiCalls';
 import getPokemonDetails from '../../apiData/cleanApiCalls';
 import './PokemonDetails.css';
 
@@ -14,7 +14,7 @@ class PokemonDetails extends Component {
 
   componentDidMount = async () => {
     try {
-      const fetchedPokemonDetails = await getIndividualPokemon(this.state.id)
+      const fetchedPokemonDetails = await fetchPokemonData(`/${this.state.id}`)
       this.setState({ pokemonDetails: getPokemonDetails(fetchedPokemonDetails)})
     } catch (e) {
       this.setState({error: 'Request failed'})
@@ -28,7 +28,9 @@ class PokemonDetails extends Component {
       {pokemonDetails &&
         <section>
           <h1>{pokemonDetails.name}</h1>
-          <img src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} alt={pokemonDetails.name} />
+          <img 
+          src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} 
+          alt={pokemonDetails.name} />
           <p>Weight: {pokemonDetails.weight}</p>
           <p>Height: {pokemonDetails.height}</p>
           <p>Types: {pokemonDetails.types}</p>
