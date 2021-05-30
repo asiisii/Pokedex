cb6d6f7fef130aece07af4b147e337e5ae5ba5cd
Ashish Malla <ashishmalla45@gmail.com>
2021-05-30 07:14:24 -0600

    Capitalize pokemon name

--- src/apiData/cleanApiCalls.js
+++ src/apiData/cleanApiCalls.js
@@ -1,6 +1,14 @@
 const getPokemonDetails = (data) => {
+  const { abilities, height, name, moves, types, weight } = data
   console.log(data);
-  
+  const capitalName = name.charAt(0).toUpperCase() + name.slice(1)
+  const typesInfo = types.map(type => type.type.name)
+  console.log(typesInfo);
+  return {
+    name: capitalName,
+    height: height,
+    weight: weight,
+  }
 }
 
 
