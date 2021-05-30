c303be15f5ae8f4e62c60279f027df652dd06522
Rachel Hendricks <rachelhen92@gmail.com>
2021-05-30 15:34:30 -0600

    Add div tag and change src

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom';
 import uncaughtBall from '../../Assets/uncaughtBall.png';
 import caughtBall from  '../../Assets/caughtBall.png'
 
-const PokemonCard = ({id, img, name}) => {
+const PokemonCard = ({id, img, name, pokeball}) => {
   return (
-    <Link to={`/${id}`} className="pokemon-card">
-      <button className="pokeball"><img src={uncaughtBall} alt="Pokeball" /></button>
-      <img
-      className="pokemon-pic"
-      id = {id}
-      src={img}
-      alt={`${name} posing`}
-      />
-      <h1>{name}</h1>
-    </Link>
+    <div className="pokemon-card">
+      <button className="pokeball"><img src={pokeball} alt="Pokeball" /></button>
+      <Link to={`/${id}`} >
+        <img
+        className="pokemon-pic"
+        id = {id}
+        src={img}
+        alt={`${name} posing`}
+        />
+        <h1>{name}</h1>
+      </Link>
+    </div>
   )
 }
 
