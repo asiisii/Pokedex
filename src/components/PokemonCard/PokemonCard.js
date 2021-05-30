027b4747c642717ef3d30db1de80b2c3824aaf0a
Rachel Hendricks <rachelhen92@gmail.com>
2021-05-30 16:16:18 -0600

    Add onClick event to button

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom';
 import uncaughtBall from '../../Assets/uncaughtBall.png';
 import caughtBall from  '../../Assets/caughtBall.png'
 
-const PokemonCard = ({id, img, name, pokeball}) => {
+const PokemonCard = ({id, img, name, pokeball, favorite}) => {
   return (
     <div className="pokemon-card">
-      <button className="pokeball"><img src={pokeball} alt="Pokeball" /></button>
+      <button className="pokeball" onClick={() => {
+        favorite(name);
+      }}>
+      <img src={pokeball} alt="Pokeball" />
+      </button>
       <Link to={`/${id}`} >
         <img
         className="pokemon-pic"
