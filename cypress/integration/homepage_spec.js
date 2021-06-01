6583c6524d4148b8c901c47201169aa26a3c08f0
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-01 12:49:02 -0400

    Add test to check card 1

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -7,4 +7,14 @@ describe("Home Page", () => {
   it('should be able to see Pokemon cards', () => {
     cy.get('.card-display').children().should('have.length', 9)
   })
+
+  it('1st card has name of Bulbasaur', () => {
+    cy.get('.card-display')
+      .get('.pokemon-card')
+      .eq(0)
+      .contains('bulbasaur')
+      .find('img')
+      .should('have.attr', 'src', 'https://pokeres.bastionbot.org/images/pokemon/1.png')
+      .get('#1')
+  })
 })
