a18be256bff2a462b9b9808e9d23034a854139c9
Ashish Malla <ashishmalla45@gmail.com>
2021-06-01 16:38:49 -0600

    Add test for bulbasaur info page

--- cypress/integration/detailpage_spec.js
+++ cypress/integration/detailpage_spec.js
@@ -18,11 +18,28 @@ describe('Detail page', () => {
       .get('a').eq(1).contains('Show Caught')
   })
 
-  it.only('should have info header', () => {
+  it('should have info header', () => {
     cy.get('.info-header > a')
       .contains('Go back')
       .get('h1').contains('bulbasaur')
       .get('img').eq(0)
       .should('have.attr', 'src', uncaughtBall)
+  })
+
+  it.only('should have bulbasaur info', () => {
+    cy.get('.pokemon-info')
+      .find('img').eq(1)
+      .should('have.attr', 'src', 'https://pokeres.bastionbot.org/images/pokemon/1.png')
+      .get('p').eq(0)
+      .contains('Weight: 69')
+      .get('p').eq(1)
+      .contains('Height: 7')
+      .get('p').eq(2)
+      .contains('Types: grass')
+      .get('p').eq(3)
+      .contains('Abilities: overgrow')
+      .get('p').eq(4)
+      .contains('Moves: razor-wind | swords-dance')
+    
   })
 })
