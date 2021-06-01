d0478039aef42ae6a99df582430e6ef30c03ec6b
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-01 11:52:24 -0600

    Add test to check for navbar

--- cypress/integration/caughtpage_spec.js
+++ cypress/integration/caughtpage_spec.js
@@ -6,4 +6,11 @@ describe('Caught Page', () => {
   it('should show url change to caught pokemon section', () => {
     cy.url().should('eq', 'http://localhost:3000/caught')
   })
+
+  it('should show navbar on load', () => {
+    cy.get('header')
+      .get('a').eq(0).contains('Home')
+      .get('a').eq(1).contains('Show Caught')
+      .get('h1').contains('Pokédex')
+  })
 })
