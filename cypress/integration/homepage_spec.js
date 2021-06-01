7f59685ef8e942f8ddd06b4f7de2593459c327d5
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-01 12:10:01 -0400

    Add dummy data

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -2,4 +2,8 @@ describe("Home Page", () => {
   beforeEach(() => {
     cy.visit('/')
   })
+
+  it('should be able to see Pokemon cards', () => {
+    cy.get('.card-display').children().should('have.length', 151)
+  })
 })
