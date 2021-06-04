dad5279b1dd30ad637758ce164f89e2629bd2a5f
Ashish Malla <ashishmalla45@gmail.com>
2021-06-04 08:40:56 -0600

    Update keys with env reference

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyAwA1Il0oVP0qgEhinAs2tKzVIZDhXh16s',
-  authDomain: 'pokedex-development.firebaseapp.com',
-  projectId: 'pokedex-development',
-  storageBucket: 'pokedex-development.appspot.com', 
-  messagingSenderId: '592485036172',
-  appId: '1:592485036172:web:9e4e57895ad242e557cba9',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET, 
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 })
 
 const auth = app.auth()
