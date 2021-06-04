54d43efc72a13d9dee8b5ac7f3c77fbbe363d166
Ashish Malla <ashishmalla45@gmail.com>
2021-06-04 08:19:36 -0600

    Add a method to update the user state

--- src/components/LoginConfig/LoginConfig.js
+++ src/components/LoginConfig/LoginConfig.js
@@ -46,7 +46,17 @@ class LoginConfig extends React.Component {
     auth.signOut()
   }
 
-  
+  changeAuthState() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({user: user})
+      } else {
+        this.setState({user: ''})
+      }
+    })
+  }
+
+
 }
 
 export default LoginConfig
