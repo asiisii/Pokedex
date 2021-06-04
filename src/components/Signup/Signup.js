e52f5e538b4b88cd7388e92fed5d08d1dda5c73f
Ashish Malla <ashishmalla45@gmail.com>
2021-06-04 08:12:27 -0600

    Add method to update email state

---
+++ src/components/Signup/Signup.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { auth } from '../../firebase'
+import Login from '../Login/Login'
+import { Link } from 'react-router-dom'
+class LoginConfig extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: '',
+      email: '',
+      password: '',
+      error: '',
+    //  isUser: false
+    }
+  }
+
+  setEmail(e) {
+    e.preventDefault()
+    this.setState({email: e.target.value})
+  }
+
+  
+}
+
+export default LoginConfig
