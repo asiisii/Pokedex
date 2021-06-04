89201325fe0683dcef2de754a924d828827a3183
Dustin-Har <harbaughdustin@yahoo.com>
2021-06-04 16:10:15 -0400

    Connect Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREVASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREVASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREVASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREVASE_STORAGE_BUCKET, 
-  messagingSenderId: process.env.REACT_APP_FIREVASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREVASE_APP_ID,
+  apiKey: "AIzaSyCmhBHkWqiO4NAtSk7bIgVSzQ5dSk46xeY",
+  authDomain: "pokedex-development-3f7ce.firebaseapp.com",
+  projectId: "pokedex-development-3f7ce",
+  storageBucket: "pokedex-development-3f7ce.appspot.com",
+  messagingSenderId: "381629222597",
+  appId: "1:381629222597:web:486b637ba14d45a66caaac"
 })
 
 export const auth = app.auth()
