9a902fbbb98fdc32df0168b78c3249e3de0b2dc1
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-05 11:37:43 -0600

    Add test to make sure login form renders

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -3,4 +3,14 @@ describe('Login Page', () => {
     cy.visit('http://localhost:3000/login')
   });
 
+  it('should render the login form', () => {
+    cy.contains('h2', 'Continue Adventure')
+      .get('label').eq(0).contains('Email')
+      .get('form input[type="email"]')
+      .get('label').eq(1).contains('Password')
+      .get('form input[type="password"]')
+      .get('button').contains('Login')
+      .get('p').contains('Need an account?')
+      .get('a').contains('Sign Up')
+  })
 })
