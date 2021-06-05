d048808775138067695857ba1851430752f04c64
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-05 11:42:59 -0600

    Add test to check if value is being updated

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -1,7 +1,7 @@
 describe('Login Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/login')
-  });
+  })
 
   it('should render the login form', () => {
     cy.contains('h2', 'Continue Adventure')
@@ -12,5 +12,12 @@ describe('Login Page', () => {
       .get('button').contains('Login')
       .get('p').contains('Need an account?')
       .get('a').contains('Sign Up')
+  })
+
+  it('should be able to type in inputs and see value updated', () => {
+    cy.get('form input[type="email"]').type('test@gmail.com')
+      .should('have.value', 'test@gmail.com')
+      .get('form input[type="password"]').type('123456')
+      .should('have.value', '123456')
   })
 })
