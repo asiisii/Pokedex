30bbc19976a8f961184642dae9321423d7e763b9
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-07 09:43:09 -0600

    Remove logout test from individual pokemon page

--- cypress/integration/detailpage_spec.js
+++ cypress/integration/detailpage_spec.js
@@ -81,9 +81,4 @@ describe('Detail page', () => {
       .contains('Moves: swords-dance | cut | bind')
   })
 
-  it('should logout account once Log out button is clicked', () => {
-    cy.get('button').contains('Log out').click()
-      .url().should('eq', 'http://localhost:3000/login')
-      .get('h2').contains('Continue Adventure')
-  })
 })
